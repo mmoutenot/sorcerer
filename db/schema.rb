64582e07b24d976ef3980948b2fa77c644f42f38
aldenks <aldenkeefesampson@gmail.com>
2014-03-18 21:17:15 -0400

    generate model Vote topic_source_id:integer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140319011240) do
+ActiveRecord::Schema.define(version: 20140319011619) do
 
   create_table "sources", force: true do |t|
     t.string   "title"
@@ -30,6 +30,12 @@ ActiveRecord::Schema.define(version: 20140319011240) do
 
   create_table "topics", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "votes", force: true do |t|
+    t.integer  "topic_source_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
