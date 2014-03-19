644ac8452229647e51f17a3bf1549cc33ab4b663
aldenks <aldenkeefesampson@gmail.com>
2014-03-18 22:13:23 -0400

    Add initial seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+topic = Topic.create :name => 'Stalin'
+source1 = Source.create :title => 'Stalin\'s Callin',
+                        :author => 'Joe Stephenoplis',
+                        :isbn => '1929401113'
+source2 = Source.create :title => 'My Little Red Diary',
+                        :author => 'Joe Stalin',
+                        :isbn => '111111111'
+topic.sources = [source1, source2]
+topic_sources = TopicSource.where :topic => topic, :source => source1
+vote = Vote.create :topic_source => topic_sources.first
